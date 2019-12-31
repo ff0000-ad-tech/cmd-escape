4d8c99d9011c82d7ccac500b4432d363d19f77b5
Greg Connell <greg.connell@wearered.com>
2019-12-31 10:11:02 -0800

    adds prettierrc

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function cmdEscape(args) {
-  return args
-    .map(arg => {
-      arg = new String(arg);
-      if (!arg.match(/^--/)) {
-        arg = arg.replace(/"/g, '\\"');
-        return `"${arg}"`;
-      } else {
-        return arg;
-      }
-    })
-    .join(" ");
+	return args
+		.map(arg => {
+			arg = new String(arg)
+			if (!arg.match(/^--/)) {
+				arg = arg.replace(/"/g, '\\"')
+				return `"${arg}"`
+			} else {
+				return arg
+			}
+		})
+		.join(' ')
 }
-module.exports = cmdEscape;
+module.exports = cmdEscape
