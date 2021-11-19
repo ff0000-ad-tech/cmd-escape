4a7706cad067cfc43f3b7738217416b79f7fd811
Greg Connell <gconnell@160over90.com>
2021-11-19 15:40:22 -0800

    accommodate args with =

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-function cmdEscape(args) {
+const cmdEscape = args => {
+	const esc = arg => `"${arg.replace(/"/g, '\\"')}"`
 	return args
 		.map(arg => {
 			arg = new String(arg)
-			if (!arg.match(/^--/)) {
-				arg = arg.replace(/"/g, '\\"')
-				return `"${arg}"`
+			const isKey = arg.match(/^--/)
+			const kv = arg.split('=')
+			if (isKey) {
+				return kv.length > 1 ? `${kv[0]}=${esc(kv[1])}` : arg
 			} else {
-				return arg
+				return kv.length > 1 ? `${kv[0]}=${esc(kv[1])}` : esc(arg)
 			}
 		})
 		.join(' ')
 }
+
 module.exports = cmdEscape
